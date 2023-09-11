e143dc6b3c4648a0044349a77534b9bef089c2ad
heroshg <heros.goodoy@gmail.com>
2023-09-11 17:14:01 -0300

    Arquivos finais desafio

---
+++ Contador.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			
+			System.err.println("Erro" + e.getMessage());
+		}
+		terminal.close();
+	}
+	
+	public static class ParametrosInvalidosException extends Exception {
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public ParametrosInvalidosException(String mensagem) {
+			super(mensagem);
+		}
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		
+		if(parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+		
+		int contagem = parametroDois - parametroUm;
+		
+		for(int i = 0; i < contagem; i++) {
+			System.out.println("Imprimindo o numero " + (i + 1));
+		}
+	}
+}
